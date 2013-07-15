666113201ac9349a41ad6af26f9a1700070a36bd
saltpy <saltpy@gmail.com>
2013-07-15 15:25:52 +0100

    Completed build automation and linting steps

--- Jakefile.js
+++ Jakefile.js
@@ -1,33 +1,38 @@
 /*global desc, task, jake, fail, complete */
 
-"use strict";
+(function() {
+    "use strict";
 
-task("default", ["lint"]);
+    task("default", ["lint"]);
 
-desc("Lint everything");
-task("lint", [], function() {
-    var lint = require("./build/lint/lint_runner.js");
+    desc("Lint everything");
+    task("lint", [], function() {
+        var lint = require("./build/lint/lint_runner.js");
 
-    var files = new jake.FileList();
-    files.include("**/*.js");
-    files.exclude("node_modules");
+        var files = new jake.FileList();
+        files.include("**/*.js");
+        files.exclude("node_modules");
 
-    var options = {
-        bitwise:true,
-        curly:false,
-        eqeqeq:true,
-        forin:true,
-        immed:true,
-        latedef:true,
-        newcap:true,
-        noarg:true,
-        noempty:true,
-        nonew:true,
-        regexp:true,
-        undef:true,
-        strict:true,
-        trailing:true,
-        node:true 
-    };
-    lint.validateFileList(files.toArray(), options, {});
-});
+        lint.validateFileList(files.toArray(), nodeLintOptions(), {});
+    });
+
+    function nodeLintOptions() {
+            return {
+                bitwise:true,
+                curly:false,
+                eqeqeq:true,
+                forin:true,
+                immed:true,
+                latedef:true,
+                newcap:true,
+                noarg:true,
+                noempty:true,
+                nonew:true,
+                regexp:true,
+                undef:true,
+                strict:true,
+                trailing:true,
+                node:true 
+        };
+    }
+})();
