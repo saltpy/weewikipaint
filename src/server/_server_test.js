32764b4fed7d6223f7bd70d8a10bdbc2c16cb028
saltpy <saltpy@gmail.com>
2013-07-16 16:44:21 +0100

    Improve server test a little

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -3,6 +3,6 @@
 var server = require("./server.js");
 
 exports.testNothing = function(test) {
-    test.equals(server.number(), 0, "number");
+    test.equals(0, server.number(), "number");
     test.done();
 };
