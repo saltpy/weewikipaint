9fcd64d1ea4a5a7923b193672073d5dab4206737
saltpy <saltpy@gmail.com>
2013-07-22 10:16:34 +0100

    Refactor to use express

--- src/server/server.js
+++ src/server/server.js
@@ -1,17 +1,20 @@
 "use strict";
 
+var express = require("express");
 var http = require("http");
 var server;
+var app;
 
 exports.start = function(port) {
     if(!port) throw "Require port number";
-    server = http.createServer();
 
-    server.on("request", function(request, response) {
-        response.end("Hello World");
+    app = express();
+
+    app.get("/", function(req, res) {
+        res.end("Hello World");
     });
-    
-    server.listen(port);
+
+    server = app.listen(port);
 };
 
 exports.stop = function(callback) {
