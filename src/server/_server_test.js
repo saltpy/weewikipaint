ce36b60ae57922eb49db20ff91625df356a20064
saltpy <saltpy@gmail.com>
2013-07-22 16:49:04 +0100

    Finish fixing the test that shows a file can be served

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -7,46 +7,36 @@ var fs = require("fs");
 
 var TEST_STATIC_DIR = "generated/static";
 
-var fileCleanUp = function(callback) {
-    syscmd("rm -rf " + TEST_STATIC_DIR).on("exit", function() {
-        callback();
-    });
-};
-
 exports.setUp = function(callback) {
     var server = app.start(8080, TEST_STATIC_DIR, callback);
 };
 
 exports.tearDown = function(callback) {
-    app.stop(callback);
+    syscmd("rm -rf " + TEST_STATIC_DIR).on("exit", function() {
+        app.stop(callback);
+    });
 };
 
-//exports.test_servesHomePageFromFile = function(test) {
-//    var expected = "Test homepage file";
-//    fs.writeFileSync(TEST_STATIC_DIR + '/index.html', expected);
-//
-//    app.start(8080, TEST_STATIC_DIR);
-//
-//    var req = http.get('http://localhost:8080');
-//    req.on("response", function(response) {
-//        var data = "";
-//        response.setEncoding("utf8");
-//
-//        response.on("data", function(chunk) {
-//            data += chunk;
-//        });
-//
-//        response.on("end", function() {
-//            app.stop(function() {
-//                test.equals(200, response.statusCode);
-//                test.equals(data, data);
-//                exports.fileCleanUp(function() {
-//                    test.done();
-//                });
-//            });
-//        });
-//    });
-//};
+exports.test_servesHomePageFromFile = function(test) {
+    var expected = "Test homepage file";
+    fs.writeFileSync(TEST_STATIC_DIR + '/index.html', expected);
+    
+    var request = http.get('http://localhost:8080');
+    request.on("response", function(response) {
+        var data = false;
+        test.equals(200, response.statusCode);
+
+        response.on("data", function(chunk) {
+            data = true;
+            test.equals(expected, chunk);
+        });
+
+        response.on("end", function() {
+            test.ok(data);
+            test.done();
+        });
+    });
+};
 
 exports.testServerThrowsExceptionWhenCalledWithNoPortNumber = function(test) {
     test.throws(function() {
